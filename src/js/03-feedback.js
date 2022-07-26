19fd75249727670f8a0ab1114c88672707d2842d
Lisnyi <99568987+Lisnyi@users.noreply.github.com>
2022-07-26 17:07:45 +0300

    task 3, version 0.1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
 import throttle from 'lodash.throttle'
+
+const form = document.querySelector(".feedback-form");
+const submitButton = document.querySelector("button")
+
+function addFeedback (e) {
+    if (e.target.tagName === "INPUT") {
+        try {
+            localStorage.setItem("feedback-form-state", JSON.stringify(`{"email": ${e.target.value}}`));
+          } catch (error) {
+            console.error("Set state error: ", error.message);
+          }
+    } else if (e.target.tagName === "TEXTAREA") {
+        try {
+            localStorage.setItem("feedback-form-state", JSON.stringify(`{"email": ,"message": ${e.target.value}}`));
+            console.log(localStorage.getItem("feedback-form-state"))
+          } catch (error) {
+            console.error("Set state error: ", error.message);
+          }
+    }
+
+}
+
+form.addEventListener("input", throttle(addFeedback, 500))
+
+
+// loginButton.addEventListener("click", () => {
+//     if (form[0].value.trim() === "" || form[1].value.trim() === "") {
+//         alert("Всі поля повинні бути заповнені!")
+//     } else {
+//     form.addEventListener('submit', onFormSubmit); 
+
+//     function onFormSubmit(event) {
+//     event.preventDefault();
+
+//     const email = event.currentTarget.elements.email.value
+//     const password = event.currentTarget.elements.password.value
+
+//     const formData = {
+//         email,
+//         password
+//     }
+    
+//         console.log(formData)
+        
+//         event.currentTarget.reset()
+// }}})
