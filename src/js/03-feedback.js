92ac0a93062e887134c33a6439e009a76763900f
Lisnyi <99568987+Lisnyi@users.noreply.github.com>
2022-07-27 09:22:07 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,38 @@
 import throttle from 'lodash.throttle'
 
 const form = document.querySelector(".feedback-form");
-const submitButton = document.querySelector("button")
+let feedback = {}
 
 function addFeedback (e) {
-    if (e.target.tagName === "INPUT") {
-        try {
-            localStorage.setItem("feedback-form-state", JSON.stringify(`{"email": ${e.target.value}}`));
-          } catch (error) {
-            console.error("Set state error: ", error.message);
-          }
-    } else if (e.target.tagName === "TEXTAREA") {
-        try {
-            localStorage.setItem("feedback-form-state", JSON.stringify(`{"email": ,"message": ${e.target.value}}`));
-            console.log(localStorage.getItem("feedback-form-state"))
-          } catch (error) {
-            console.error("Set state error: ", error.message);
-          }
+  if (e.target.value.trim() !== "") {
+    feedback[e.target.name] = e.target.value.trim()
+    try {
+      localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+    } catch (error) {
+      console.error("Set state error: ", error.message);
     }
+  }
+}
 
+function onFormSubmit (e) {
+  e.preventDefault()
+  e.currentTarget.reset()
+
+  console.log (feedback)
+}
+
+function checkLocalStorage () {
+  const feedbackFromStorage = localStorage.getItem("feedback-form-state")
+  if (feedbackFromStorage !== null)
+  try {
+    feedback = JSON.parse(feedbackFromStorage);
+    !feedback.email ? '' : form[0].value = feedback.email
+    !feedback.message ? '' : form[1].value = feedback.message
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
 }
 
 form.addEventListener("input", throttle(addFeedback, 500))
-
-
-// loginButton.addEventListener("click", () => {
-//     if (form[0].value.trim() === "" || form[1].value.trim() === "") {
-//         alert("Всі поля повинні бути заповнені!")
-//     } else {
-//     form.addEventListener('submit', onFormSubmit); 
-
-//     function onFormSubmit(event) {
-//     event.preventDefault();
-
-//     const email = event.currentTarget.elements.email.value
-//     const password = event.currentTarget.elements.password.value
-
-//     const formData = {
-//         email,
-//         password
-//     }
-    
-//         console.log(formData)
-        
-//         event.currentTarget.reset()
-// }}})
+form.addEventListener("submit", onFormSubmit)
+window.addEventListener("load", checkLocalStorage)
